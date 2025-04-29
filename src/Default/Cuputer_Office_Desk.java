b23199efd393a7b7d5956f834d72e0f24f795319
CosmoSouza <lucascap9@gmail.com>
2025-04-29 01:21:01 -0300

    Update Cuputer_Office_Desk.java

--- src/Default/Cuputer_Office_Desk.java
+++ src/Default/Cuputer_Office_Desk.java
@@ -1,35 +1,39 @@
-package Default;
+package Inadimplencia;
+
 import java.util.Locale;
-public class Cuputer_Office_Desk {
+import java.util.Scanner;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		 String product1 = "Computer";
-		 String product2 = "Office desk";
-		 
-		 byte age = 30;
-		 int code = 5290;
-		 char gender = 'F';
-		 
-		 double price1 = 2100.0;
-		 double price2 = 650.50;
-		 double measure = 53.234567;
-		 
-		 
-		System.out.println("Pruduct:");
-		System.out.printf("%s, which price is $ %.2f%n", product1, price1);
-		System.out.printf("%s, which price is $ %.2f%n", product2, price2);
-		System.out.println();
-		System.out.printf("Record: %d years old, code %d and gender: %c%n", age, code, gender);
-		System.out.println();
-		System.out.printf("measeu with eithg decimal places: %.8f%n", measure);
-		System.out.printf("rouded (three decimal places): %.3f%n", measure);
-		Locale.setDefault(Locale.US);
-		System.out.printf("US decimal point: %.3f%n", measure);
-	
-		
-		
-		 
-	}
+public class Computer_Office_Desk {
 
+    public static void main(String[] args) {
+
+        // Variáveis
+        String produto1 = "Computador";
+        String produto2 = "Mesa de escritório";
+
+        byte idade = 30;
+        int codigo = 5290;
+        char genero = 'F';
+
+        double preco1 = 2100.0;
+        double preco2 = 650.50;
+        double medida = 53.234567;
+
+        // Impressão dos produtos
+        System.out.println("Produtos:");
+        System.out.printf("%s, cujo preço é R$ %.2f%n", produto1, preco1);
+        System.out.printf("%s, cujo preço é R$ %.2f%n", produto2, preco2);
+        System.out.println();
+
+        // Impressão de cadastro
+        System.out.printf("Registro: %d anos, código %d e gênero: %c%n", idade, codigo, genero);
+        System.out.println();
+
+        // Impressão de medida com diferentes formatos
+        System.out.printf("Medida com oito casas decimais: %.8f%n", medida);
+        System.out.printf("Arredondado (três casas decimais): %.3f%n", medida);
+        
+        Locale.setDefault(Locale.US);
+        System.out.printf("Separador decimal dos EUA: %.3f%n", medida);
+    }
 }
